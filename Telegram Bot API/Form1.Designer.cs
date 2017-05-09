99d95442be619ab8a38b47ae27752d2876743047
Koperfild <Avalancher2@rambler.ru>
2017-05-09 23:44:00 +0300

    can send message back

--- Telegram Bot API/Form1.Designer.cs
+++ Telegram Bot API/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(81, 142);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(111, 55);
+            this.SendButton.TabIndex = 0;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(81, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start receiving";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 128);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 319);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SendButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Telegram Bot API/Form1.cs
+++ Telegram Bot API/Form1.cs
@@ -3,18 +3,99 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 
 namespace Telegram_Bot_API
 {
     public partial class Form1 : Form
     {
+        HashSet<ChatId> chat_ids = new HashSet<ChatId>();
+        string fileToSaveChatsIds = "ChatsToSendMessages.txt";
+        public static readonly TelegramBotClient Client = new TelegramBotClient("385714306:AAG134HuQnlJtqopZXYP3QlsJPG8kykNdxE");
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Client.StartReceiving();
+            Client.OnMessage += OnMessageHandler;
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            var update = Client.GetUpdatesAsync().Result;
+            if (!update.Any())
+                return;
+            var chat_id = update.FirstOrDefault()?.Message.Chat.Id;
+            if (chat_id == null)
+                return;
+            chat_ids.Add(chat_id);
+            if (!chat_ids.Any())
+                return;
+            
+        }
+
+        private void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var message = e.Message;
+            var chat_id = message.Chat.Id;
+            chat_ids.Add(chat_id);
+            chat_ids = new HashSet<ChatId>(chat_ids.Distinct(new ChatIdComparer()));
+            //chat_ids.Distinct(new IEqualityComparer)
+            XmlSerializer ser = new XmlSerializer(typeof(HashSet<string>));
+            StreamWriter writer;
+            if (!System.IO.File.Exists(this.fileToSaveChatsIds))
+                writer = new StreamWriter(System.IO.File.Create(this.fileToSaveChatsIds));
+            else
+                writer = new StreamWriter(this.fileToSaveChatsIds);
+
+            writer.WriteLine((string)chat_id);
+            writer.Dispose();
+            
+        }
+        public class ChatIdComparer : IEqualityComparer<ChatId>
+        {
+            public bool Equals(ChatId x, ChatId y)
+            {
+                var a1 = (string)x;
+                var a2 = (string)y;
+                return a1 == a2;
+            }
+
+            public int GetHashCode(ChatId obj)
+            {
+                return 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Client.StopReceiving();
+        }
+
+        private void SendButton_Click_1(object sender, EventArgs e)
+        {
+            foreach(var chatId in this.chat_ids)
+                Client.SendTextMessageAsync(chatId, "Привет далбаёб");
+        }
+    }
+    static class Bot
+    {
+        
     }
 }
